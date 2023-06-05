6869b9c2246e9d56b768792a8d88bcdd41327511
johnweissmann <131970947+johnweissmann@users.noreply.github.com>
2023-06-05 17:11:25 -0400

    Finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,23 +1,18 @@
 import os
 import csv
-from statistics import mean
 
 csvpath = os.path.join("Resources", "budget_data.csv")
+textpath = os.path.join("Analysis", "analysis.txt")
 
 month = 0 # start the months at zero
 total = 0 # start the total value at zero
-# total_change = []
-greatest_increase = ["",0]
-greatest_decrease = ["",9999999999999999]
-month_of_change = []
 net_change_list = []
+greatest_increase = ["",0] # create an empty list with a value at index 1 that everything will be bigger than
+greatest_decrease = ["",9999999999999999] # do the same for decrease but with a value that everyhting will be smaller than
 
 with open(csvpath, encoding="UTF-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    # Read the header row
     next(csvreader, None) # skips the header for calculations
-
-    # Extract first row to avoid appending to net_change_list
     first_row = next(csvreader)
     month += 1
     total += int(first_row[1])
@@ -35,8 +30,28 @@ with open(csvpath, encoding="UTF-8") as csvfile:
             greatest_decrease[0]=row[0]
             greatest_decrease[1]=net_change
     net_monthly_avg = sum(net_change_list) / len(net_change_list)
-    print("Total Months:",month)
-    print("Total:","${:}".format(total))
-    print("Average Change:","${:}".format(round(net_monthly_avg,2)))
-    print("Greatest Increase in Profits:", greatest_increase[0],"${:}".format(greatest_increase[1]))
-    print("Greatest Decrease in Profits:", greatest_decrease[0],"${:}".format(greatest_decrease[1]))
+    print("Total Months: ",month)
+    print("Total: ","${:}".format(total))
+    print("Average Change: ","${:}".format(round(net_monthly_avg,2)))
+    print("Greatest Increase in Profits: ", greatest_increase[0],"${:}".format(greatest_increase[1]))
+    print("Greatest Decrease in Profits: ", greatest_decrease[0],"${:}".format(greatest_decrease[1]))
+    totalmonthssolution = ["Total Months: ",str(month)] 
+    totalsolution = ["Total: ",str("${:}".format(total))]
+    avgchangesolution = ["Average Change: ",str("${:}".format(round(net_monthly_avg,2)))]
+    incsolution = ["Greatest Increase in Profits: ", str(greatest_increase[0])," ",str("${:}".format(greatest_increase[1]))]
+    decsolution = ["Greatest Decrease in Profits: ", str(greatest_decrease[0])," ",str("${:}".format(greatest_decrease[1]))]
+with open(textpath,"w") as txt:
+    txt.writelines("Financial Analysis")
+    txt.writelines("\n")
+    txt.writelines("---------------------------")
+    txt.writelines("\n")
+    txt.writelines(totalmonthssolution)
+    txt.writelines("\n")
+    txt.writelines(totalsolution)
+    txt.writelines("\n")
+    txt.writelines(avgchangesolution)
+    txt.writelines("\n")
+    txt.writelines(incsolution)
+    txt.writelines("\n")
+    txt.writelines(decsolution)
+    txt.writelines("\n")
